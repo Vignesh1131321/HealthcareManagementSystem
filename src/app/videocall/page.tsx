0b20b162a1e1dc6bae4977e293a548ea65c72723
Vignesh Pasupuleti <vignesh.pasupulety369@gmail.com>
2025-01-15 13:55:40 +0530

    Added history to chatbot

---
+++ src/app/videocall/page.tsx
@@ -0,0 +1,12 @@
+import VideoCall from "../components/VideoCall";
+import Header from "../components/Header";
+import { NavbarWrapper } from "../healthcare/components/NavbarWrapper";
+export default function ChatbotPage() {
+    return (
+      <div>
+        <NavbarWrapper/>
+        <VideoCall />
+      </div>
+  
+    );
+  }
